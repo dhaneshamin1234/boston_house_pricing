be866e2bbee7be9d1045151f313123577896b1f3
Dhanesh Amin <dhaneshamin1234@gmail.com>
2023-09-05 21:40:02 +0530

    web application

--- app.py
+++ app.py
@@ -32,5 +32,15 @@ def predict_api():
     return jsonify(output[0])
 
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = rfr_model.predict(final_input)[0]
+    # print('test', rfr_model.predict(final_input))
+    return render_template("home.html", prediction_text="The House price prediction is {}".format(output))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
